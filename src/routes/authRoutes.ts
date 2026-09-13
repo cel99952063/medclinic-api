0ec4f78a30cd89e72cceb30dd50253e3900589fd
cel99952063 <cel99952063@gmail.com>
2026-09-12 21:52:44 -0300

    feat: implementa autenticação via login e JWT

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController.js";
+import { AuthController } from "../controllers/AuthController.js";
 
 const authRoutes = Router();
 const userController = new UserController();
+const authController = new AuthController();
 
-// Rota cadastro de usuários (RF03)
+// Rota cadastro de usuários (RF03 rf07)
 authRoutes.post("/auth/register", userController.create);
+authRoutes.post("/auth/login", authController.login);
 
 export { authRoutes };
